2aeb0be079fdb143537530f22c8d89608c97435b
Nils <80416356+nils-dambowy@users.noreply.github.com>
2025-01-01 22:57:24 +0100

    added second project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,93 +1,45 @@
 import Header from "./Header"
 import Footer from "./Footer"
-import Placeholder from  "../assets/GRAPH.png"
+import Placeholder from "../assets/GRAPH.png"
 
 export default function Projects() {
-    return (
-        <>
-            <Header/>
-            <div className="flex h-full lg:h-screen  sm:h-full w-full items-right lg:flow-root justify-evenly">
-                {/* project sections*/}
-                <div className=" w-full lg:w-4/5 sm:w-full float-left">
-                    <div className="items-center flex lg:flex-row lg:flex-wrap flex-col justify-evenly pt-8 gap-8 px-4">
+  return (
+    <>
+      <Header />
+      <div className="flex h-full lg:h-screen  sm:h-full w-full items-right lg:flow-root justify-evenly">
+        {/* project sections*/}
+        <div className=" w-full lg:w-4/5 sm:w-full float-left">
+          <div className="items-center flex lg:flex-row lg:flex-wrap flex-col justify-evenly pt-8 gap-8 px-4">
 
-                        <a href="projects/1" className="project-grid-item bg-white border-black border-2 w-full sm:w-1/2 lg:w-1/4 h-64">
-                            <div>
-                                <div className="flex flex-row h-3/4 text-center">
-                                    <img src={Placeholder} alt="Placeholder" width="450"/>
-                                </div>
-                                <div className="flex flex-row border-black border-t-2 h-1/4 justify-center">
-                                    openbouldermap
-                                </div>
-                            </div>
-                        </a>
-                        {/*
-                        <a href="projects/2" className="project-grid-item bg-white border-black border-2 w-full sm:w-1/2 lg:w-1/4 h-64">
-                            <div>
-                                <div className="flex flex-row h-3/4 text-center">
-                                    <img src={Placeholder} alt="Placeholder" width="450"/>
-                                </div>
-                                <div className="flex flex-row border-black border-t-2 h-1/4 justify-center">
-                                    Project 2
-                                </div>
-                            </div>
-                        </a>
-                        */}
-                    </div>
-                    {/*
-                    <div className="items-center flex lg:flex-row lg:flex-wrap flex-col justify-evenly pt-8 gap-8 px-4">
-                        <a href="projects/3" className="project-grid-item bg-white border-black border-2 w-full sm:w-1/2 lg:w-1/4 h-64">
-                            <div>
-                                <div className="flex flex-row h-3/4 text-center">
-                                    <img src={Placeholder} alt="Placeholder" width="450"/>
-                                </div>
-                                <div className="flex flex-row border-black border-t-2 h-1/4 justify-center">
-                                    Project 3
-                                </div>
-                            </div>
-                        </a>
-                        <a href="projects/4" className="project-grid-item bg-white border-black border-2 w-full sm:w-1/2 lg:w-1/4 h-64">
-                            <div>
-                                <div className="flex flex-row h-3/4 text-center">
-                                    <img src={Placeholder} alt="Placeholder" width="450"/>
-                                </div>
-                                <div className="flex flex-row border-black border-t-2 h-1/4 justify-center">
-                                    Project 4
-                                </div>
-                            </div>
-                        </a>
-                    </div>
-                    */}
-                    {/*
-                    <div className="items-center flex lg:flex-row lg:flex-wrap flex-col justify-evenly pt-8 gap-8 pb-8 px-4 ">
-                        <a href="projects/5" className="project-grid-item bg-white border-black border-2 w-full sm:w-1/2 lg:w-1/4 h-64">
-                            <div>
-                                <div className="flex flex-row h-3/4 text-center">
-                                    <img src={Placeholder} alt="Placeholder" width="450"/>
-                                </div>
-                                <div className="flex flex-row border-black border-t-2 h-1/4 justify-center">
-                                    Project 5
-                                </div>
-                            </div>
-                        </a>
-                        <a href="projects/6" className="project-grid-item bg-white border-black border-2 w-full sm:w-1/2 lg:w-1/4 h-64">
-                            <div>
-                                <div className="flex flex-row h-3/4 text-center">
-                                    <img src={Placeholder} alt="Placeholder" width="450"/>
-                                </div>
-                                <div className="flex flex-row border-black border-t-2 h-1/4 justify-center">
-                                    Project 6
-                                </div>
-                            </div>
-                        </a>
-                    </div>
-                    */}
+            <a href="projects/1" className="project-grid-item bg-white border-black border-2 w-full sm:w-1/2 lg:w-1/4 h-64">
+              <div>
+                <div className="flex flex-row h-3/4 text-center">
+                  <img src={Placeholder} alt="Placeholder" width="450" />
                 </div>
+                <div className="flex flex-row border-black border-t-2 h-1/4 justify-center">
+                  openbouldermap
+                </div>
+              </div>
+            </a>
 
-                {/* Grid background*/}
-                <div className="work-grid-bg flex-col  w-0 lg:w-1/5 h-full float-right"></div>
-            </div>
-            <Footer/>
-        </>
-    );
+            <a href="projects/2" className="project-grid-item bg-white border-black border-2 w-full sm:w-1/2 lg:w-1/4 h-64">
+              <div>
+                <div className="flex flex-row h-3/4 text-center">
+                  <img src={Placeholder} alt="Placeholder" width="450" />
+                </div>
+                <div className="flex flex-row border-black border-t-2 h-1/4 justify-center">
+                  URL shortener
+                </div>
+              </div>
+            </a>
+
+          </div>
+        </div>
+
+        {/* Grid background*/}
+        <div className="work-grid-bg flex-col  w-0 lg:w-1/5 h-full float-right"></div>
+      </div>
+      <Footer />
+    </>
+  );
 }
