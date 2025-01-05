3be774ab2543c088a65bf56b2bf3a113a585ffe3
Nils <nils.dambowy@googlemail.com>
2025-01-05 17:13:17 +0100

    style: changed the contact me text

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,38 +1,28 @@
 import Header from "./Header";
 import Footer from "./Footer";
-import Arrow from "../assets/arrow.png";
 
 export default function Contact() {
-    return (
-        <>
-            <Header />
-            <div className="flex flex-col items-center justify-center h-screen w-screen px-10">
-                <div id="contact-card" className="w-44 h-20 flex items-center justify-center mt-12 bg-gray-200 text-3xl">
-                    Contact
-                </div>
-                <div className="flex flex-row items-center mt-8">
-                    <img src={Arrow} className="w-48 h-48 rotated" />
-                    <div id="contact-card" className="w-44 h-20 flex items-center justify-center bg-gray-200 text-3xl">
-                        me!
-                    </div>
-                </div>
-
-                <div className="lg:w-3/6 bg-white w-full justify-center flex flex-col items-center pb-5 border-black border-4 work-grid-bg mt-12">
-                    <div id="contact-card" className="w-5/6 h-20 flex items-center justify-center mt-4 text-3xl bg-gray-200">
-                        Github
-                    </div>
-                    <div id="contact-card" className="w-5/6 h-20 flex items-center justify-center mt-8 text-3xl bg-gray-200">
-                        LinkedIn
-                    </div>
-                    <div id="contact-card" className="w-5/6 h-20 flex items-center justify-center mt-8 text-3xl bg-gray-200">
-                        E-Mail
-                    </div>
-                    <div id="contact-card" className="w-5/6 h-20 flex items-center justify-center mt-8 text-3xl bg-gray-200">
-                        CV
-                    </div>
-                </div>
-            </div>
-            <Footer />
-        </>
-    );
+  return (
+    <>
+      <Header />
+      <div className="flex flex-col items-center justify-center h-screen w-screen px-10">
+        <p className=" text-9xl italic text-shadow-white">Contact me!</p>
+        <div className="lg:w-3/6 bg-white w-full justify-center flex flex-col items-center pb-5 border-black border-4 work-grid-bg mt-12">
+          <div id="contact-card" className="w-5/6 h-20 flex items-center justify-center mt-4 text-3xl bg-gray-200">
+            Github
+          </div>
+          <div id="contact-card" className="w-5/6 h-20 flex items-center justify-center mt-8 text-3xl bg-gray-200">
+            LinkedIn
+          </div>
+          <div id="contact-card" className="w-5/6 h-20 flex items-center justify-center mt-8 text-3xl bg-gray-200">
+            E-Mail
+          </div>
+          <div id="contact-card" className="w-5/6 h-20 flex items-center justify-center mt-8 text-3xl bg-gray-200">
+            CV
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
 }
